6173e5ac56ea5414da9262deb75ff706f7644158
Priscilla Del Toro <priscilladeltoro10@gmail.com>
2019-04-25 19:44:47 -0700

    review array

---
+++ js/review-array-methods.js
@@ -0,0 +1,21 @@
+var fruits = ['apple', 'banana', 'orange'];
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+var fruitsNotArray = 'apple','banana','orange';
+
+fruits.push('blueberry');
+console.log(fruits);
+
+fruits.unshift('pear');
+console.log(fruits);
+
+fruits.splice(2,1,'strawberry');
+console.log(fruits);
+
+var copyOfFruits = fruits.slice();
+console.log(copyOfFruits);
+
+console.log(fruits.length); //displays amount of 'fruits'
+console.log(Array.isArray(fruits)); //will return true or false value
